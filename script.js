55061e3d804431e00783479938a150f46653d83d
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-09-06 20:18:54 +0200

    add listener to submit button and created function for book card

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-function Book(title, author, pageCount, readCondition) {
+function Book(title, author, pageCount) {
     this.title = title;
     this.author = author;
     this.pageCount = pageCount;
-    this.readCondition = readCondition;
     this.info = function () {
-        return this.title + ", " + this.author + ", " + this.pageCount + ", " + this.readCondition;
+        const bookInfo = [this.title, this.author, this.pageCount];
+        return bookInfo;        
     };
 };
 
 function addBookToLibrary() {
-    const newBook = new Book(title, author, pageCount, readCondition);
-    myLibrary += newBook;
+    let title = bookTitle.value;
+    let author = bookAuthor.value;
+    let pageCount = parseInt(numberOfPages.value);
+    const newBook = new Book(title, author, pageCount);
+    createBookCard(title, author, pageCount);
+    myLibrary.push(newBook.info());
 };
+
+function createBookCard(title, author, pageCount) {
+    let card = document.createElement("div")
+    card.className = "card";
+    cardContainer.appendChild(card);
+}
 
 const myLibrary = [];
 
@@ -19,10 +29,26 @@ const addBtn = document.getElementById("add-btn")
 const contentContainer = document.getElementById("content-body");
 const sidebarContainer = document.getElementById("sidebar");
 let form = document.querySelector("form");
+let bookTitle = document.getElementById("book-title");
+let bookAuthor = document.getElementById("book-author");
+let numberOfPages = document.getElementById("page-count");
+let submitBtn = document.getElementById("submit-btn");
+let cardContainer = document.getElementById("book-cards");
 
 addBtn.addEventListener("click", () => {
-    console.log("add button clicked");
     addBtn.remove();
-    contentContainer.style.gridTemplateColumns = "2fr 4fr 1fr";
-    form.style.display = "block";
+    contentContainer.style.gridTemplateColumns = "2fr 5fr";
+    form.style.display = "flex";
+    submitBtn.style.alignSelf = "flex-start";
+    submitBtn.style.marginLeft = "2em";
+});
+
+submitBtn.addEventListener("click", ()=> {
+    addBookToLibrary();
+    form.style.display = "none";
+    contentContainer.style.gridTemplateColumns = "1fr 5fr";
+    sidebarContainer.appendChild(addBtn);
+    form.addEventListener("submit", (event)=> {
+        event.preventDefault();
+    });
 });
